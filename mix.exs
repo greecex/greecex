b6d2aa7eb02774732502f4960c575b9f7dd3aa51
Petros Amoiridis <petros@hey.com>
2025-02-20 14:40:52 +0200

    Enable sending email

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Greecex.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:hackney, "~> 1.20"}
     ]
   end
 
