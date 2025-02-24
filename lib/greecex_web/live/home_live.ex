bdeb50f4df532cfd92494389cbcec6207aa3fcb4
Petros Amoiridis <petros@hey.com>
2025-02-24 11:02:34 +0200

    Tweak home page copy

--- lib/greecex_web/live/home_live.ex
+++ lib/greecex_web/live/home_live.ex
@@ -9,7 +9,7 @@ defmodule GreecexWeb.HomeLive do
     ~H"""
     <div class="max-w-3xl mx-auto text-center px-6">
       <h1 class="text-3xl font-extrabold text-gray-900 tracking-tight">
-        Join the community!
+        Elixir is amazing!
       </h1>
 
       <p class="mt-4 text-base text-gray-700 leading-relaxed text-justify">
@@ -24,13 +24,13 @@ defmodule GreecexWeb.HomeLive do
           navigate={~p"/subscribe"}
           class="rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
         >
-          Subscribe
+          Get community updates
         </.link>
       </div>
 
       <div class="mt-8 text-left space-y-4">
         <h2 class="text-base font-semibold text-gray-900">
-          Why join?
+          Why subscribe to updates?
         </h2>
       </div>
 
@@ -104,14 +104,14 @@ defmodule GreecexWeb.HomeLive do
       <div class="bg-purple-50 mt-12 mb-2 rounded-md">
         <div class="mx-auto max-w-7xl px-6 py-12 sm:py-16 lg:flex lg:items-center lg:justify-between lg:px-8">
           <h2 class="max-w-2xl text-2xl font-semibold tracking-tight text-gray-900 sm:text-xl">
-            Feeling major FOMO! <br />Subscribe me to updates.
+            Feeling major FOMO right? <br />Let's build the community together.
           </h2>
           <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
             <.link
               navigate={~p"/subscribe"}
               class="rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
             >
-              Subscribe
+              Do it!
             </.link>
             <.link navigate={~p"/about"} class="text-sm/6 font-semibold text-gray-900">
               Learn more <span aria-hidden="true">→</span>
