cb3bba99bbfff7dd31a740d8371863615dbdcaea
Petros Amoiridis <petros@hey.com>
2025-02-24 07:56:51 +0200

    Confirms a subscriber via their email link

--- lib/greecex_web/live/confirm_live.ex
+++ lib/greecex_web/live/confirm_live.ex
@@ -1,13 +1,25 @@
 defmodule GreecexWeb.ConfirmLive do
   use GreecexWeb, :live_view
+  alias Greecex.Subscribers
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(%{"token" => token}, _session, socket) do
+    case Subscribers.confirm_subscriber(token) do
+      {:ok, _subscriber} ->
+        {:ok, assign(socket, :status, :confirmed)}
+
+      {:error, _reason} ->
+        {:ok, assign(socket, :status, :invalid)}
+    end
   end
 
   def render(assigns) do
     ~H"""
-    <p>Thank you for confirming</p>
+    <%= case @status do %>
+      <% :confirmed -> %>
+        <p>Thank you for confirming your subscription!</p>
+      <% :invalid -> %>
+        <p>Sorry, that confirmation link is invalid or has expired.</p>
+    <% end %>
     """
   end
 end
---
+++ test/greecex_web/live/confirm_live_test.ex
@@ -0,0 +1,30 @@
+defmodule GreecexWeb.SubscribeLiveTest do
+  use GreecexWeb.ConnCase
+  alias Greecex.Subscribers
+  import Phoenix.LiveViewTest
+
+  describe "ConfirmLive" do
+    test "confirms a subscriber with a valid token", %{conn: conn} do
+      {:ok, subscriber} =
+        Subscribers.create_subscriber(%{"email" => "test@example.com", "city" => "Agrinio"})
+
+      assert subscriber.confirmed_at == nil
+      {:ok, view, _html} = live(conn, ~p"/confirm/#{subscriber.confirmation_token}")
+      assert render(view) =~ "Thank you for confirming"
+      subscriber = Subscribers.get_subscriber_by_email("test@example.com")
+      assert subscriber.confirmed_at != nil
+    end
+
+    test "does not confirm a subscriber with an invalid token", %{conn: conn} do
+      {:ok, subscriber} =
+        Subscribers.create_subscriber(%{"email" => "test@example.com", "city" => "Agrinio"})
+
+      assert subscriber.confirmed_at == nil
+      {:ok, view, _html} = live(conn, ~p"/confirm/invalid_token")
+      assert render(view) =~ "confirmation link"
+      assert render(view) =~ "is invalid"
+      subscriber = Subscribers.get_subscriber_by_email("test@example.com")
+      assert subscriber.confirmed_at == nil
+    end
+  end
+end
