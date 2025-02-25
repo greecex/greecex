c0fa27950352bc81309397cbbfc218923543393e
Petros Amoiridis <petros@hey.com>
2025-02-25 17:19:57 +0200

    List subscribers in the admin dashboard

--- lib/greecex_web/live/admin/dashboard_live.ex
+++ lib/greecex_web/live/admin/dashboard_live.ex
@@ -1,10 +1,14 @@
 defmodule GreecexWeb.Admin.DashboardLive do
   use GreecexWeb, :live_view
 
+  alias Greecex.Subscribers
+
   @impl true
   def mount(_params, _session, socket) do
     if admin?(socket.assigns.current_user) do
-      {:ok, socket}
+      subscribers = Subscribers.list_subscribers()
+      count = Enum.count(subscribers)
+      {:ok, assign(socket, subscribers: subscribers, count: count)}
     else
       {:ok, redirect(socket, to: "/404")}
     end
@@ -13,9 +17,35 @@ defmodule GreecexWeb.Admin.DashboardLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="container">
-      <h1>Admin Dashboard</h1>
+    <div class="prose">
+      <h1>Admin dashboard</h1>
       <p>Welcome to the admin dashboard!</p>
+
+      <h2>Subscribers ({@count})</h2>
+      <ul>
+        <%= for subscriber <- @subscribers do %>
+          <li class="text-sm">
+            <.link href={
+                    "mailto:#{subscriber.email}?subject=" <>
+                    URI.encode("Welcome to Greece |> Elixir") <>
+                    "&body=" <>
+                    URI.encode("Hello #{subscriber.email},\n\nPlease confirm your email address to never miss an update:\n\n#{Subscribers.get_subscription_confirmation_url(subscriber)}\n\nCheers,\nPetros @ Greece |> Elixir")
+                  }>
+              {subscriber.email}
+            </.link>
+          </li>
+          <ul class="text-xs">
+            <li>Subscribed on: {subscriber.inserted_at}</li>
+            <li>Confirmation link: {Subscribers.get_subscription_confirmation_url(subscriber)}</li>
+            <li>{if subscriber.confirmed_at, do: "Confirmed", else: "Pending confirmation"}</li>
+            <li>City: {subscriber.city}</li>
+            <li>Elixir experience: {subscriber.elixir_experience}</li>
+            <li>
+              Willing to co-organize: {if subscriber.willing_to_coorganize, do: "Yes", else: "No"}
+            </li>
+          </ul>
+        <% end %>
+      </ul>
     </div>
     """
   end
