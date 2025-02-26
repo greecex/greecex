fe8ab9f2d2934c6b4d01ae065e258562d2979bef
Jerry Cheung <jollyjerry@gmail.com>
2025-02-26 11:56:57 -0800

    rm redundant padding
    so it aligns with rest of page

--- lib/greecex_web/live/home_live.ex
+++ lib/greecex_web/live/home_live.ex
@@ -34,7 +34,7 @@ defmodule GreecexWeb.HomeLive do
         </h2>
       </div>
 
-      <div class="mx-auto mt-4 max-w-7xl px-6 sm:mt-4 md:mt-4 lg:px-8 text-left">
+      <div class="mx-auto mt-4 max-w-7xl sm:mt-4 md:mt-4 text-left">
         <dl class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-6 text-sm text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-8">
           <div class="relative pl-9">
             <dt class="inline font-semibold text-gray-900">
