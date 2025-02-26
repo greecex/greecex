3453e760584ca287862e68ad50c140356c3045b1
Jerry Cheung <jollyjerry@gmail.com>
2025-02-26 11:57:32 -0800

    center Do it!

--- lib/greecex_web/live/home_live.ex
+++ lib/greecex_web/live/home_live.ex
@@ -106,7 +106,7 @@ defmodule GreecexWeb.HomeLive do
           <h2 class="max-w-2xl text-2xl font-semibold tracking-tight text-gray-900 sm:text-xl">
             Feeling major FOMO right? <br />Let's build the community together.
           </h2>
-          <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
+          <div class="mt-10 flex items-center justify-center gap-x-6 lg:mt-0 lg:shrink-0">
             <.link
               navigate={~p"/subscribe"}
               class="rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
